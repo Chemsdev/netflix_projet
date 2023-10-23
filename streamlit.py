15bc1a6b873a399d7967dac0f2e8884bdd3439f7
Ounissi Chems <chemscreative@gmail.com>
2023-10-23 10:18:30 +0200

    formulaire

--- streamlit.py
+++ streamlit.py
@@ -6,13 +6,16 @@ def main():
     st.write("Hello")
 
     with st.form("my_form"):
-        st.write("Inside the form")
-        slider_val = st.slider("Form slider")
-        checkbox_val = st.checkbox("Form checkbox")
-
+        st.write("Veuillez remplir le formulaire")
+        input1 = st.text_input("Champ 1")
+        input2 = st.text_input("Champ 2")
+        input3 = st.text_input("Champ 3")
+        input4 = st.text_input("Champ 4")    
         submitted = st.form_submit_button("Envoyer")
         if submitted:
             st.write("Les prédictions :")
+            # Ajoutez ici le code pour traiter les prédictions en fonction des valeurs des champs.
+
 
 
 main()
