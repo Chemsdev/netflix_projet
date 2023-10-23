62388d7a165e56230e7b05103bfaf0da81e22e2d
Ounissi Chems <chemscreative@gmail.com>
2023-10-23 10:45:56 +0200

    mise en place formulaire

--- streamlit.py
+++ streamlit.py
@@ -5,16 +5,19 @@ from db import *
 def main():
     st.write("Hello")
 
+    # Le formulaire.
     with st.form("my_form"):
         st.write("Veuillez remplir le formulaire")
         
+        # Dictionnaire contenant les inputs.
         dictionnaire = {
             "input1" : st.text_input("Champ 1"),
             "input2" : st.text_input("Champ 2"),
             "input3" : st.text_input("Champ 3"),
             "input4" : st.text_input("Champ 4")    
         }
-
+        
+        # Envoi du formuaire.
         submitted = st.form_submit_button("Envoyer")
         if submitted:
             
